6a257fe2403b36f65f7a70755161cd3ca0e571a0
aminnairi <18418459+aminnairi@users.noreply.github.com>
2025-10-18 20:59:34 +0200

    now allowing the aggregate to update the user

--- core/aggregates/user.ts
+++ core/aggregates/user.ts
@@ -1,6 +1,7 @@
 import { randomUUID } from "crypto";
 import { UserFact } from "@application/core/facts/user/fact";
-import { UserCreatedFact } from "@application/core/facts/user/user-created";
+import { UserCreatedFactV1 } from "../facts/user/user-created-v1";
+import { UserUpdatedFactV1 } from "../facts/user/user-updated-v1";
 
 export class UserAggregate {
   public constructor(
@@ -8,24 +9,46 @@ export class UserAggregate {
     public readonly email: string = "",
     public readonly password: string = "",
     public readonly confirmed: boolean = false,
+    public readonly version = 0
   ) { }
 
-  public static fromFacts(events: UserFact[]) {
-    return events.reduce((oldUser, event) => {
-      if (event.name === "user-created") {
-        return new UserAggregate(event.data.identifier, event.data.email, event.data.password, event.data.confirmed)
+  public static fromFacts(facts: UserFact[]) {
+    return facts.reduce((oldUser, fact) => {
+      if (fact.name === "user-created-v1") {
+        return new UserAggregate(fact.data.identifier, fact.data.email, fact.data.password, fact.data.confirmed)
+      }
+
+      if (!oldUser) {
+        return oldUser
+      }
+
+      if (fact.name === "user-updated-v1") {
+        return new UserAggregate(
+          fact.data.identifier,
+          fact.data.email ?? oldUser?.email,
+          oldUser.password,
+          oldUser.confirmed,
+          oldUser.version + 1
+        )
       }
 
       return oldUser
     }, null as UserAggregate | null)
   }
 
-  public static create(email: string, password: string) {
-    return new UserCreatedFact({
+  public static create(version: number, email: string, password: string) {
+    return new UserCreatedFactV1(version, {
       identifier: randomUUID(),
       email,
       password,
       confirmed: false
+    })
+  }
+
+  public update(email?: string) {
+    return new UserUpdatedFactV1(this.version, {
+      identifier: this.identifier,
+      email: email ?? this.email
     })
   }
 }
