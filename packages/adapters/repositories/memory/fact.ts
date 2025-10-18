6cee94b490c0b7025ada3b096ee43ea184dea399
aminnairi <18418459+aminnairi@users.noreply.github.com>
2025-10-18 21:04:51 +0200

    now using a map to ensure optimistic locking in memory

--- packages/adapters/repositories/memory/fact.ts
+++ packages/adapters/repositories/memory/fact.ts
@@ -1,28 +1,33 @@
+import { ConcurrencyError } from "@application/core/errors/concurrency"
 import { DomainFact } from "@application/core/facts/domain"
-import { UserCreatedFact } from "@application/core/facts/user/user-created"
+import { UserFact } from "@application/core/facts/user/fact"
 import { FactRepository } from "@application/core/repositories/fact"
 import { QueryRepository } from "@application/core/repositories/query"
 
 export class MemoryFactRepository implements FactRepository {
-  public constructor(private readonly events: DomainFact[] = [], private readonly queries: QueryRepository[] = []) { }
+  public constructor(private readonly facts: Map<{ aggregate: DomainFact["aggregate"], identifier: string, version: number }, DomainFact> = new Map(), private readonly queries: QueryRepository[] = []) { }
 
-  public dispatch(fact: DomainFact): void {
+  public async findUserFacts(userIdentifier: string): Promise<UserFact[]> {
+    return Array.from(this.facts.values()).filter(fact => {
+      return fact.data.identifier === userIdentifier
+    })
+  }
+
+  public async save(fact: DomainFact): Promise<ConcurrencyError | null> {
+    if (this.facts.has({ aggregate: fact.aggregate, identifier: fact.data.identifier, version: fact.version })) {
+      return new ConcurrencyError
+    }
+
+    this.facts.set({ aggregate: fact.aggregate, identifier: fact.data.identifier, version: fact.version }, fact)
+
     this.queries.forEach(query => {
       query.handle(fact)
     })
+
+    return null
   }
 
   public register(query: QueryRepository): void {
     this.queries.push(query)
-  }
-
-  public async save(fact: DomainFact): Promise<void> {
-    this.events.push(fact)
-  }
-
-  public async findUserEventsByIdentifier(identifier: string): Promise<UserCreatedFact[]> {
-    return this.events.filter(fact => {
-      return fact.aggregate === "user" && fact.identifier === identifier
-    })
   }
 }
