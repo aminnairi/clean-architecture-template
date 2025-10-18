3c1d12eb3db8fcb6b2b6552e427f712470596e5a
aminnairi <18418459+aminnairi@users.noreply.github.com>
2025-10-18 21:00:05 +0200

    hashing the password and checking for errors

--- core/commands/register.ts
+++ core/commands/register.ts
@@ -1,18 +1,27 @@
 import { UserAggregate } from "@application/core/aggregates/user";
 import { FactRepository } from "@application/core/repositories/fact";
 import { NotificationService } from "@application/core/services/notification";
+import { PasswordService } from "../services/password";
 
 export class RegisterCommand {
   public constructor(
     private readonly eventRepository: FactRepository,
+    private readonly passwordService: PasswordService,
     private readonly notificationService: NotificationService
   ) { }
 
-  public async execute(email: string, password: string) {
-    const user = UserAggregate.create(email, password)
+  public async execute(request: { email: string, password: string }) {
+    const version = 0
+    const password = await this.passwordService.hash(request.password)
+    const user = UserAggregate.create(version, request.email, password)
 
-    this.eventRepository.dispatch(user)
-    this.notificationService.notifyRegistrationSucceeded()
+    const error = await this.eventRepository.save(user)
+
+    if (error) {
+      return error
+    }
+
+    await this.notificationService.notifyRegistrationSucceeded()
 
     return user.data.identifier
   }
