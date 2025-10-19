f2b3c05d804895ec1f3527223298c9530bf11e19
aminnairi <18418459+aminnairi@users.noreply.github.com>
2025-10-19 22:10:02 +0200

    matching all facts to exhaustively handle them

--- packages/adapters/repositories/memory/query.ts
+++ packages/adapters/repositories/memory/query.ts
@@ -1,9 +1,18 @@
 import { UserAggregate } from "@application/core/aggregates/user"
 import { DomainFact } from "@application/core/facts/domain"
 import { QueryRepository } from "@application/core/repositories/query"
+import { match } from "@application/library/match"
 
 export class MemoryQueryRepository implements QueryRepository {
   public constructor(private readonly users: UserAggregate[] = []) { }
+
+  public async findUserByConfirmationToken(confirmationToken: string): Promise<UserAggregate | null> {
+    const user = this.users.find(user => {
+      return user.confirmationToken === confirmationToken
+    })
+
+    return user ?? null
+  }
 
   public async findUserByIdentifier(identifier: string): Promise<UserAggregate | null> {
     return this.users.find(user => {
@@ -12,23 +21,54 @@ export class MemoryQueryRepository implements QueryRepository {
   }
 
   public handle(fact: DomainFact): void {
-    if (fact.name === "user-created-v1") {
-      this.users.push(new UserAggregate(fact.aggregateIdentifier, fact.data.email, fact.data.password, fact.data.confirmed, fact.version))
-      return
-    }
+    match(fact, {
+      "user-confirmed-v1": userConfirmedFact => {
+        const userIndex = this.users.findIndex(user => {
+          return user.identifier === userConfirmedFact.aggregateIdentifier
+        })
 
-    if (fact.name === "user-updated-v1") {
-      const userIndex = this.users.findIndex(user => {
-        return user.identifier === fact.aggregateIdentifier
-      })
+        if (userIndex < 0) {
+          return
+        }
 
-      if (!userIndex) {
-        return
+        this.users.splice(userIndex, 1, new UserAggregate(
+          userConfirmedFact.aggregateIdentifier,
+          this.users[userIndex].email,
+          this.users[userIndex].password,
+          null,
+          userConfirmedFact.version,
+          this.users[userIndex].createdAt,
+          userConfirmedFact.data.updatedAt
+        ))
+      },
+      "user-created-v1": userCreatedFact => {
+        this.users.push(new UserAggregate(
+          userCreatedFact.aggregateIdentifier,
+          userCreatedFact.data.email,
+          userCreatedFact.data.password,
+          userCreatedFact.data.confirmationToken,
+          userCreatedFact.version
+        ))
+      },
+      "user-updated-v1": userUpdatedFact => {
+
+        const userIndex = this.users.findIndex(user => {
+          return user.identifier === fact.aggregateIdentifier
+        })
+
+        if (userIndex < 0) {
+          return
+        }
+
+        this.users.splice(userIndex, 1, new UserAggregate(
+          userUpdatedFact.aggregateIdentifier,
+          userUpdatedFact.data.email,
+          this.users[userIndex].password,
+          this.users[userIndex].confirmationToken,
+          userUpdatedFact.version
+        ))
       }
-
-      this.users.splice(userIndex, 1, new UserAggregate(fact.aggregateIdentifier, fact.data.email, this.users[userIndex].password, this.users[userIndex].confirmed, fact.version))
-      return
-    }
+    })
   }
 
   public async findUserByEmail(email: string): Promise<UserAggregate | null> {
