15e0909963276bdb2ec52a2c20c0fd1c2b078dd9
aminnairi <18418459+aminnairi@users.noreply.github.com>
2025-10-19 20:27:49 +0200

    find user query now implements the query interface

--- core/queries/find-user.ts
+++ core/queries/find-user.ts
@@ -1,11 +1,16 @@
 import { QueryRepository } from "@application/core/repositories/query";
+import { Query } from ".";
+import { FindUserRequest } from "@application/core/request/find-user";
+import { FindUserResponse } from "@application/core/response/find-user";
 
-export class FindUserQuery {
+export class FindUserQuery implements Query<FindUserRequest, FindUserResponse> {
   public constructor(private readonly queryRepository: QueryRepository) { }
 
-  public async execute(identifier: string) {
-    const user = await this.queryRepository.findUserByIdentifier(identifier)
+  public async fetch(request: FindUserRequest): Promise<FindUserResponse> {
+    const user = await this.queryRepository.findUserByIdentifier(request.identifier)
 
-    return user
+    return {
+      user
+    }
   }
 }
