284ff3ef4e653fa513335384d578cd13ed624eed
aminnairi <18418459+aminnairi@users.noreply.github.com>
2025-10-19 22:11:10 +0200

    added the confirmation fact

--- packages/core/facts/user/fact.ts
+++ packages/core/facts/user/fact.ts
@@ -1,6 +1,8 @@
 import { UserUpdatedFactV1 } from "./user-updated-v1";
+import { UserConfirmedV1 } from "./user-confirmed-v1";
 import { UserCreatedFactV1 } from "./user-created-v1";
 
 export type UserFact =
   | UserCreatedFactV1
   | UserUpdatedFactV1
+  | UserConfirmedV1
