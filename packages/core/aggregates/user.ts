051ec96db55dab9bb41d8aca9ab0618e6f0f0a61
aminnairi <18418459+aminnairi@users.noreply.github.com>
2025-10-19 22:10:19 +0200

    new methods for all things related to confirmation

--- packages/core/aggregates/user.ts
+++ packages/core/aggregates/user.ts
@@ -4,13 +4,14 @@ import { UserCreatedFactV1 } from "../facts/user/user-created-v1";
 import { UserUpdatedFactV1 } from "../facts/user/user-updated-v1";
 import { Aggregate } from ".";
 import { match } from "@application/library/match"
+import { UserConfirmedV1 } from "../facts/user/user-confirmed-v1";
 
 export class UserAggregate implements Aggregate {
   public constructor(
     public readonly identifier: string = "",
     public readonly email: string = "",
     public readonly password: string = "",
-    public readonly confirmed: boolean = false,
+    public readonly confirmationToken: string | null = null,
     public readonly version = 0,
     public readonly createdAt = new Date(),
     public readonly updatedAt = new Date()
@@ -24,7 +25,7 @@ export class UserAggregate implements Aggregate {
             userCreatedFact.aggregateIdentifier,
             userCreatedFact.data.email,
             userCreatedFact.data.password,
-            userCreatedFact.data.confirmed
+            userCreatedFact.data.confirmationToken,
           )
         },
         "user-updated-v1": userUpdatedFact => {
@@ -36,26 +37,52 @@ export class UserAggregate implements Aggregate {
             userUpdatedFact.aggregateIdentifier,
             userUpdatedFact.data.email ?? oldUser?.email,
             oldUser.password,
-            oldUser.confirmed,
+            oldUser.confirmationToken,
             oldUser.version + 1
+          )
+        },
+        "user-confirmed-v1": userConfirmedFact => {
+          if (!oldUser) {
+            return oldUser
+          }
+
+          return new UserAggregate(
+            oldUser.identifier,
+            oldUser.email,
+            oldUser.password,
+            null,
+            oldUser.version + 1,
+            oldUser.createdAt,
+            userConfirmedFact.data.updatedAt
           )
         }
       })
     }, null as UserAggregate | null)
   }
 
-  public static create(version: number, email: string, password: string) {
+  public static create(email: string, password: string, confirmationToken: string) {
     const aggregateIdentifier = randomUUID()
+    const version = 0
 
     return new UserCreatedFactV1(version, aggregateIdentifier, {
       email,
       password,
-      confirmed: false
+      confirmationToken,
     })
   }
 
+  public confirm() {
+    return new UserConfirmedV1(this.version + 1, this.identifier, {
+      updatedAt: new Date()
+    })
+  }
+
+  public isConfirmed() {
+    return this.confirmationToken === null
+  }
+
   public update(email?: string) {
-    return new UserUpdatedFactV1(this.version, this.identifier, {
+    return new UserUpdatedFactV1(this.version + 1, this.identifier, {
       email: email ?? this.email
     })
   }
